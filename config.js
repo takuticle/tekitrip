2dd1446d49813100e4d3a78073790e408ef83d81
Takuya NAGATA <takuticle@users.noreply.github.com>
2025-12-07 03:28:27 +0900

    APIキーを変更

--- config.js
+++ config.js
@@ -2,5 +2,5 @@
 // === config.js ===
 // APIキーはここだけに書く（index.html や app.js に直書きしない）
 window.APP_CONFIG = {
-  GOOGLE_MAPS_API_KEY: "AIzaSyCIBS-JGki1tKPjcfrpyiWWcp7X8Jbs4oU"
+  GOOGLE_MAPS_API_KEY: "AIzaSyD4_FszwSGnbeuSBBf98XzJrBHCJunJUKw"
 };
